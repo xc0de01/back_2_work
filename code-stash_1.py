7f6a33b3a281ddd5d2845946aca6693347279e1c
xc0de01 <adedayomiajibade2004@gmail.com>
2025-08-18 12:51:18 +0100

    Commit 1

---
+++ code-stash_1.py
@@ -0,0 +1,19 @@
+# project1-temperature-converter
+
+
+temp_var = {'Celsius':'*C','Kelvin':'*K', 'Fahrenheit': '*F'}
+key_list = []
+for key in temp_var:
+    key_list.append(key)
+temp_unit = input("Enter the temperature unit measure:").title()
+while True:
+    if temp_unit not in temp_var:
+        print('measure unknown')
+        temp_unit = input(" Enter the temperature unit measure:").title()
+    else:
+        print('okay')
+        break
+
+t_value = eval(input("Enter the temperature value:"))
+print(f"Final parameter: {t_value}{temp_var[temp_unit]}")
+
